0fefa11dbfc0e718651897656bf5491ec54e3ada
roeljohn <bobisroeljohn@gmail.com>
2021-03-15 10:01:08 +0800

    add the container with form in the searchform.php

--- page.php
+++ page.php
@@ -8,20 +8,7 @@ get_header();
 ?>
 
 <main id="main" class="main-wrapper" role="main">
-<section class="cta-section theme-bg-light py-5">
-		<div class="container text-center">
-			<h2 class="heading">DevBlog - A Blog Template Made For Developers</h2>
-				<div class="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div>
-				<?php get_search_form(); ?>
-			    <form class="signup-form form-inline justify-content-center pt-3">
-                    <div class="form-group">
-                        <label class="sr-only" for="semail">Your email</label>
-                        <input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Search</button>
-                </form>
-		</div><!--//container-->
-	</section>
+	<?php get_search_form(); ?>
 	<section class="blog-list px-3 py-5 p-md-5">
 			<div class="container">
 				<?php
