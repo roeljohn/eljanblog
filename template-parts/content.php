1468d483cc829c4fcf993601f8a35280794cfef1
roeljohn <bobisroeljohn@gmail.com>
2021-03-16 22:37:51 +0800

    deleted g letter on href data

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,7 +15,7 @@ $comments = get_comments($args);
 			<h3 class="title mb-1"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
 			<div class="meta mb-1"><span class="date">Published 2 months ago</span><span class="time">10 min read</span><span class="comment"><a href="<?php echo get_comment_link( $comments[0]->comment_ID ); ?>"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></a></span></div>
 			<div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-			<a class="more-link" href="<?php the_permalink();?>g">Read more &rarr;</a>
+			<a class="more-link" href="<?php the_permalink();?>">Read more &rarr;</a>
 		</div><!--//media-body-->
 	</div><!--//media-->
 </div><!--//item-->
