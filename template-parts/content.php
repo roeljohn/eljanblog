0849bb27e71c4bc9aa338d635ce1f5a97a2daf6e
roeljohn <bobisroeljohn@gmail.com>
2021-03-16 20:13:50 +0800

    fix comment link in each post

--- template-parts/content.php
+++ template-parts/content.php
@@ -2,15 +2,18 @@
 /**
  * Template part for displaying posts
  */
-
+$args = array(
+    'number' => '1',
+    'post_id' => $post->ID, // use post_id, not post_ID
+);
+$comments = get_comments($args);
 ?>
-
 <div class="item mb-5">
 	<div class="media">
 		<img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url(); ?>" alt="image">
 		<div class="media-body">
 			<h3 class="title mb-1"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-			<div class="meta mb-1"><span class="date">Published 2 months ago</span><span class="time">10 min read</span><span class="comment"><a href="<?php $comments = get_comments(array('status'=>'approve')); echo get_comment_link($comments[0]->comment_ID);?>"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></a></span></div>
+			<div class="meta mb-1"><span class="date">Published 2 months ago</span><span class="time">10 min read</span><span class="comment"><a href="<?php echo get_comment_link( $comments[0]->comment_ID ); ?>"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></a></span></div>
 			<div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
 			<a class="more-link" href="<?php the_permalink();?>g">Read more &rarr;</a>
 		</div><!--//media-body-->
