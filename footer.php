f27c52adc18fe7028016539c7aa04baaf1c7fd9e
roeljohn <bobisroeljohn@gmail.com>
2021-03-18 08:51:11 +0800

    change some colors and layout. its a simple changing layout to make it more simple. i dont like too many colors

--- footer.php
+++ footer.php
@@ -5,16 +5,13 @@
  */
 
 ?>
-	    <footer class="footer text-center py-2 theme-bg-dark">
-		   
-           <!--/* This template is released under the Creative Commons Attribution 3.0 License. Please keep the attribution link below when using for your own project. Thank you for your support. :) If you'd like to use the template without the attribution, you can buy the commercial license via our website: themes.3rdwavemedia.com */-->
-               <small class="copyright">Designed with <i class="fas fa-heart" style="color: #fb866a;"></i> by <a href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers</small><br/>
-               <small class="copyright">Develop and Customize by Roel John Bobis</small>
-       </footer>
-           <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/jquery-3.3.1.min.js"></script>
+    <footer class="footer text-center py-2 theme-bg-dark">
+        <small class="copyright">Develop and Customize by Roel John Bobis</small>
+    </footer>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/jquery-3.3.1.min.js"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/popper.min.js"></script> 
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/bootstrap/js/bootstrap.min.js"></script> 
-       <?php wp_footer(); ?>
-   </div><!--//main-wrapper-->
+    <?php wp_footer(); ?>
+</div><!--//main-wrapper-->
 
 
