e667a6a11d1458e01fa7624e27c8223e4ee22346
roeljohn <bobisroeljohn@gmail.com>
2021-03-18 09:45:36 +0800

    filter the excerpt by characters limit

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,7 +14,7 @@ $comments = get_comments($args);
 		<div class="media-body">
 			<h3 class="title mb-1"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
 			<div class="meta mb-1"><span class="date">Published <?php echo get_the_date( '' ); ?></span><span class="comment"><a href="<?php echo get_comment_link( $comments[0]->comment_ID ); ?>"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></a></span></div>
-			<div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
+			<div class="intro"><?php the_excerpt();?></div>
 			<a class="more-link" href="<?php the_permalink();?>">Read more &rarr;</a>
 		</div><!--//media-body-->
 	</div><!--//media-->
